7148165ecf2ee9bb987164cdcf958b89336e060a
Aashishebab <aashish.bharadwaj01@gmail.com>
2019-11-27 13:05:30 -0500

    Use camera to capture images

--- app/src/main/java/com/akp/ceg4110/quickreports/AddIncidentActivity.java
+++ app/src/main/java/com/akp/ceg4110/quickreports/AddIncidentActivity.java
@@ -20,6 +20,8 @@ import java.util.ArrayList;
 
 public class AddIncidentActivity extends AppCompatActivity{
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -31,40 +33,22 @@ public class AddIncidentActivity extends AppCompatActivity{
         }
     }
 
-    public void pickFromGallery(View view){
-        //Create an Intent with action as ACTION_PICK
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        // Sets the type as image/*. This ensures only components of type image are selected
-        intent.setType("image/*");
-        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
-        String[] mimeTypes = { "image/jpeg", "image/png" };
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-        // Launching the Intent
-        startActivityForResult(intent, 123);
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        // Result code is RESULT_OK only if the user selects an Image
-        if(resultCode == Activity.RESULT_OK){
-            if(requestCode == 123){//data.getData return the content URI for the selected Image
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = { MediaStore.Images.Media.DATA };
-                // Get the cursor
-                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-                // Move to first row
-                cursor.moveToFirst();
-                //Get the column index of MediaStore.Images.Media.DATA
-                int columnIndex = cursor.getColumnIndex(filePathColumn[ 0 ]);
-                //Gets the String value in the column
-                String imgDecodableString = cursor.getString(columnIndex);
-                cursor.close();
-                // Set the Image in ImageView after decoding the String
-                LinearLayout theImages = findViewById(R.id.uploaded_images_layout);
-                ImageView anImage = new ImageView(this);
-                anImage.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
-                theImages.addView(anImage);
-
-            }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap)extras.get("data");
+            LinearLayout theImages = findViewById(R.id.uploaded_images_layout);
+            ImageView theImage = new ImageView(this);
+            theImage.setImageBitmap(imageBitmap);
+            theImages.addView(theImage);
         }
     }
 }
