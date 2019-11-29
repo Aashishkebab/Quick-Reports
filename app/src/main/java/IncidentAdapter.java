9ec73dd1230f9b709be4104f9da4b5fa01378312
Aashishebab <aashish.bharadwaj01@gmail.com>
2019-11-29 18:14:16 -0500

    Remove menu

---
+++ app/src/main/java/IncidentAdapter.java
@@ -0,0 +1,6 @@
+package PACKAGE_NAME;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class IncidentAdapter extends RecyclerView.Adapter{
+}
--- app/src/main/java/com/akp/ceg4110/quickreports/MainActivity.java
+++ app/src/main/java/com/akp/ceg4110/quickreports/MainActivity.java
@@ -1,6 +1,7 @@
 package com.akp.ceg4110.quickreports;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,29 +30,7 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        DatabaseAccessor db = new DatabaseAccessor(this.openOrCreateDatabase(DatabaseAccessor.DATABASE_NAME, MODE_PRIVATE, null));
+        findViewById(R.id.list_of_incidents).a
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if(id == R.id.action_settings){
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    //TODO Do stuff
 }
