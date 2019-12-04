1418dee54116d358e7039d6ec5532ebcf78f4aa6
Aashishebab <aashish.bharadwaj01@gmail.com>
2019-12-03 21:25:10 -0500

    Add comments

--- app/src/main/java/com/akp/ceg4110/quickreports/AddIncidentActivity.java
+++ app/src/main/java/com/akp/ceg4110/quickreports/AddIncidentActivity.java
@@ -365,44 +365,66 @@ public class AddIncidentActivity extends AppCompatActivity{
             BitmapFactory.Options bmOptions = new BitmapFactory.Options();
             bmOptions.inJustDecodeBounds = true;
 
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
-
-
             // Decode the image file into a Bitmap sized to fill the View
             bmOptions.inJustDecodeBounds = false;
 
-            //Get the display size
+            // Get the display size
             DisplayMetrics displayMetrics = new DisplayMetrics();
             getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-            int height = displayMetrics.heightPixels;
-            int width = displayMetrics.widthPixels;
+            int height = displayMetrics.heightPixels;   // Height of screen
+            int width = displayMetrics.widthPixels; // Width of screen
 
             Bitmap imageBitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
             try{
-                if(imageBitmap.getHeight() == Math.max(imageBitmap.getHeight(), imageBitmap.getWidth())){
+                // Figure out if taller or wider
+                if(imageBitmap.getHeight() == Math.max(imageBitmap.getHeight(), imageBitmap.getWidth())){   //If taller
 
+                    // Scale width to be size we need when displaying (which just so happens to be width / 3 - 20)
+                    // Then, scale down height by the same factor/ratio so that image isn't stretched.
+                    // To do this, we figure out how by how much we divided the width, then divide height by that amount
+                    // This gives a really good approximation, but is off due to integer arithmetic being lossy
+                    // We could cast to doubles before doing the math, but that wouldn't serve any practical purpose
                     imageBitmap = Bitmap.createScaledBitmap(imageBitmap, width / 3 - 20,
                                                             imageBitmap.getHeight() / (imageBitmap.getWidth() / (width / 3 - 20)),
                                                             false);
-                }else{
+                }else{  // If wider than tall
+                    // Same process as above, but using height as the reference and scaling width accordingly
                     imageBitmap = Bitmap
                             .createScaledBitmap(imageBitmap, imageBitmap.getWidth() / (imageBitmap.getHeight() / (width / 3 - 20)),
                                                 width / 3 - 20, false);
                 }
 
+                // Crop the newly resized image
+                // Whichever was the smaller of the height and width will end up not having any crop because math
+                // We're doing the width and height each minus the same width / 3 - 20 factor as above, which is why
+                // The third and fourth parameters in this method call are the crop resolution/size
+                // For the other dimension, we wish to crop so that the *middle* part of that dimension is displayed
+                // We need to calculate the starting position, so we do that dimension (width or height) - (width / 3 - 20)
+                // Basically, we subtract off the amount we are cropping from the total width and height
+                // That gives us the *remaining* area, which we then divide by 2 to center it
+                // Dividing by 2 will shift the crop so that there's an equal amount of leftover before and after
                 imageBitmap = Bitmap.createBitmap(imageBitmap, (imageBitmap.getWidth() - (width / 3 - 20)) / 2,
                                                   (imageBitmap.getHeight() - (width / 3 - 20)) / 2, width / 3 - 20, width / 3 - 20);
+
             }catch(Exception ignored){  //Just use the full images
             }
 
             theImage.setImageBitmap(imageBitmap);
-
-            theImage.setMaxWidth(width / 3 - 20);    //Show images at 1/3rd the size for three columns
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
                                                                              LinearLayout.LayoutParams.WRAP_CONTENT);
+            // We are using a left-margin of 15 separate items in the grid
+            // Our grid holds three items per row (three columns), so we must divide the width of the screen by 3
+            // "width" is declared above as the screen width in pixels
+            // However, to compensate for the margin of 15 and prevent overflowing off the screen,
+            // we must subtract that margin from each image width
+            // However, this means the last image would end at the screen border, which would be uneven.
+            // So we add 15 / 3 to that number, since there are 3 items
+            // This way, since each image is 5 pixels smaller, it is overall 15 pixels for the entire row of 3 images
+            // This will thus leave a gap of 15 at the end, which is the same as the margin, creating a uniform appearance
             params.setMargins(15, 19, 0, 0);
             theImage.setLayoutParams(params);
+
+            theImage.setMaxWidth(width / 3 - 20);    // Show images at 1/3rd the size for three columns - see above
 
             theImagesLayout.addView(theImage);
 
