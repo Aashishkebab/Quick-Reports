bdc3ef05728302a16e41bdf81604b07ae608b4da
Aashishebab <aashish.bharadwaj01@gmail.com>
2019-12-04 22:29:39 -0500

    Open Google Photos by default

--- app/src/main/java/com/akp/ceg4110/quickreports/OpenImageListener.java
+++ app/src/main/java/com/akp/ceg4110/quickreports/OpenImageListener.java
@@ -1,6 +1,7 @@
 package com.akp.ceg4110.quickreports;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.view.View;
 
@@ -24,6 +25,13 @@ public class OpenImageListener implements View.OnClickListener{
         intent.setDataAndType(Uri.parse(this.path), "image/*");
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 //        Toast.makeText(activity.getApplicationContext(), "Please choose Google Photos if available", Toast.LENGTH_LONG).show();
-        activity.startActivity(Intent.createChooser(intent, "Please choose Google Photos (if available)"));
+        try{
+            activity.getPackageManager().getPackageInfo("com.google.android.apps.photos", 0);
+            intent.setPackage("com.google.android.apps.photos");
+            activity.startActivity(intent);
+
+        }catch(PackageManager.NameNotFoundException e){
+            activity.startActivity(Intent.createChooser(intent, "Choose the appropriate photos app (some apps may not work)"));
+        }
     }
 }
