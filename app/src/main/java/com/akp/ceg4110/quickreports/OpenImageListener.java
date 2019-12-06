c73b646a1e04da87287478d2dc0f19b6568d3b93
Aashishebab <aashish.bharadwaj01@gmail.com>
2019-12-05 20:15:56 -0500

    Super mathematical way to determine layout numbers

--- app/src/main/java/com/akp/ceg4110/quickreports/OpenImageListener.java
+++ app/src/main/java/com/akp/ceg4110/quickreports/OpenImageListener.java
@@ -23,24 +23,37 @@ public class OpenImageListener implements View.OnClickListener{
     }
 
     public static void layoutImages(int height, int width, Bitmap imageBitmap, GridLayout theImagesLayout, ImageView theImage){
-        short gridSize = 3;
+        short leftMargin = 15; // Ideally should be a multiple of all gridSizes used
+        short numberOfColumns; // The number of columns
+        short sizeOffset;   // Formula: leftMargin + leftMargin / numberOfColumns
+
+        if(width > height){ // In landscape
+            numberOfColumns = 5;
+        }else{  // In portrait, or square screen
+            numberOfColumns = 3;
+        }
+        theImagesLayout.setColumnCount(numberOfColumns);
+
+        sizeOffset = (short)(leftMargin + (leftMargin / numberOfColumns));
 
         // Figure out if taller or wider
-        if(imageBitmap.getHeight() == Math.max(imageBitmap.getHeight(), imageBitmap.getWidth())){   //If taller
+        if(imageBitmap.getHeight() == Math.max(imageBitmap.getHeight(), imageBitmap.getWidth())){   //If image taller than wide
 
             // Scale width to be size we need when displaying (which just so happens to be width / 3 - 20)
             // Then, scale down height by the same factor/ratio so that image isn't stretched.
             // To do this, we figure out how by how much we divided the width, then divide height by that amount
             // This gives a really good approximation, but is off due to integer arithmetic being lossy
             // We could cast to doubles before doing the math, but that wouldn't serve any practical purpose
-            imageBitmap = Bitmap.createScaledBitmap(imageBitmap, width / gridSize - 20,
-                                                    imageBitmap.getHeight() / (imageBitmap.getWidth() / (width / gridSize - 20)),
-                                                    false);
+            imageBitmap = Bitmap.createScaledBitmap(imageBitmap, width / numberOfColumns - sizeOffset, imageBitmap.getHeight() /
+                                                                                                       (imageBitmap.getWidth() /
+                                                                                                        (width / numberOfColumns -
+                                                                                                         sizeOffset)), false);
         }else{  // If wider than tall
             // Same process as above, but using height as the reference and scaling width accordingly
             imageBitmap = Bitmap
-                    .createScaledBitmap(imageBitmap, imageBitmap.getWidth() / (imageBitmap.getHeight() / (width / gridSize - 20)),
-                                        width / gridSize - 20, false);
+                    .createScaledBitmap(imageBitmap,
+                                        imageBitmap.getWidth() / (imageBitmap.getHeight() / (width / numberOfColumns - sizeOffset)),
+                                        width / numberOfColumns - sizeOffset, false);
         }
 
         // Crop the newly resized image
@@ -52,8 +65,9 @@ public class OpenImageListener implements View.OnClickListener{
         // Basically, we subtract off the amount we are cropping from the total width and height
         // That gives us the *remaining* area, which we then divide by 2 to center it
         // Dividing by 2 will shift the crop so that there's an equal amount of leftover before and after
-        imageBitmap = Bitmap.createBitmap(imageBitmap, (imageBitmap.getWidth() - (width / gridSize - 20)) / 2,
-                                          (imageBitmap.getHeight() - (width / gridSize - 20)) / 2, width / gridSize - 20, width / gridSize - 20);
+        imageBitmap = Bitmap.createBitmap(imageBitmap, (imageBitmap.getWidth() - (width / numberOfColumns - sizeOffset)) / 2,
+                                          (imageBitmap.getHeight() - (width / numberOfColumns - sizeOffset)) / 2,
+                                          width / numberOfColumns - sizeOffset, width / numberOfColumns - sizeOffset);
 
         theImage.setImageBitmap(imageBitmap);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -67,10 +81,10 @@ public class OpenImageListener implements View.OnClickListener{
         // So we add 15 / 3 to that number, since there are 3 items
         // This way, since each image is 5 pixels smaller, it is overall 15 pixels for the entire row of 3 images
         // This will thus leave a gap of 15 at the end, which is the same as the margin, creating a uniform appearance
-        params.setMargins(15, 19, 0, 0);
+        params.setMargins(leftMargin, 19, 0, 0);
         theImage.setLayoutParams(params);
 
-        theImage.setMaxWidth(width / gridSize - 20);    // Show images at 1/3rd the size for three columns - see above
+        theImage.setMaxWidth(width / numberOfColumns - sizeOffset);    // Show images at 1/3rd the size for three columns - see above
         // Note: The height should automatically be the same as the width, so no need to set it
 
         theImagesLayout.addView(theImage);
