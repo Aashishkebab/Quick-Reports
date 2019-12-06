2e9db639d2aee24e7ac05c5409c6a3ca85fd08c5
Aashishebab <aashish.bharadwaj01@gmail.com>
2019-12-06 01:06:42 -0500

    Delete unnecessary class

--- app/src/main/java/com/akp/ceg4110/quickreports/ui/addincident/AddIncidentViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.akp.ceg4110.quickreports.ui.addincident;
-
-import androidx.lifecycle.ViewModel;
-
-public class AddIncidentViewModel extends ViewModel{
-    // TODO: Implement the ViewModel
-}
